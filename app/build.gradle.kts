df1984eda97ceca35f4fd246400764ca057412ac
Bram <ibramsterdam@hotmail.com>
2026-03-13 20:03:03 +0100

    Add release signing config and fix R8 minification
    - Load keystore from keystore.properties (gitignored) in build.gradle.kts
    - Add signingConfig to release build type
    - Add -dontwarn rules for error-prone/guava compile-time annotations
      that R8 was failing on (javax.lang.model.element.Modifier etc.)
    - Add keystore.properties to .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+}
+
+val keystoreProperties = Properties().also { props ->
+    val file = rootProject.file("keystore.properties")
+    if (file.exists()) props.load(file.inputStream())
 }
 
 android {
@@ -15,8 +22,18 @@ android {
         versionName = "1.0.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -5,3 +5,10 @@
 -keepclassmembers class * {
     @android.webkit.JavascriptInterface <methods>;
 }
+
+# Suppress missing annotation classes from error-prone / guava that are
+# compile-time only and not present at runtime
+-dontwarn javax.lang.model.element.Modifier
+-dontwarn com.google.errorprone.annotations.**
+-dontwarn com.google.j2objc.annotations.**
+-dontwarn org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement
