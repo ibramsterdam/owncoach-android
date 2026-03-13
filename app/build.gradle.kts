14b8441a55c9fb382f8493085ac87d7205698370
Bram <ibramsterdam@hotmail.com>
2026-03-13 19:14:01 +0100

    Upgrade to latest versions: AGP 8.7.3, Kotlin 2.3.10, SDK 35, Gradle 8.11.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "app.owncoach"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "app.owncoach"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
